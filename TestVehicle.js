6ebd0feea70b66dbf187a3f619ee64285bafb578
is this the google search bar? where is my grandson thomas? <8mjoyner@stpatrick.org>
2017-08-31 14:44:29 -0500

    #1 where is my son thomas car?
    where is my son thomas, im so confused, i tried the bing and now i did the googling, but i still cant find him, please help

---
+++ TestVehicle.js
@@ -0,0 +1,12 @@
+const Vehicle = require('./Vehicle.js');
+
+function main(){
+    //make two vehicles and take them for a spin.
+let jimbovan = new vehicle(5,25,20,30,0);
+
+
+
+
+}
+//run the main method
+main();
---
+++ Vehicle.js
@@ -0,0 +1,18 @@
+let Vehicle = function(e,f,g,mpg,d){
+    let engine = e;
+    let fuel = f;
+    let gastank = g;
+    let averageMPG = mpg;
+    let mileDriven = m;
+}
+
+function totalMiles(){
+}
+function fuelLeft(){
+}
+function fill(){
+}
+function drive(){
+}
+function engineEfficency(){
+}
