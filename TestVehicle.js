f3e7e442efa43f77a0f8674120c0f780e0da30be
is this the google search bar? where is my grandson thomas? <8mjoyner@stpatrick.org>
2017-09-01 14:25:36 -0500

    #2 please internet god, where is my son thomas?
    i cant find thomas, please help me

--- TestVehicle.js
+++ TestVehicle.js
@@ -3,7 +3,7 @@ const Vehicle = require('./Vehicle.js');
 function main(){
     //make two vehicles and take them for a spin.
 let jimbovan = new vehicle(5,25,20,30,0);
-
+jimbovan.drive(1);
 
 
 
--- Vehicle.js
+++ Vehicle.js
@@ -7,12 +7,42 @@ let Vehicle = function(e,f,g,mpg,d){
 }
 
 function totalMiles(){
+  return milesDriven;
 }
 function fuelLeft(){
+  return fuel;
 }
-function fill(){
+function fill(percent){
+  fuel += percent;
+  if(fuel > 1){
+    fuel = 1;
+  }
 }
 function drive(){
+  let fuelburn = hours*engineEfficency();
+  if(fuelburn<=fuel){
+    milesDriven += (hours*averageMPG);
+    fuel -= fuelburn
+  }
+  else{
+    milesDriven = fuel*averageMPG;
+    fuel = 0;
+  }
 }
 function engineEfficency(){
+  if(engine == 4){
+    return 1;
+  }
+  else if(engine == 6){
+    return 1.2;
+  }
+  else if(engine == 8){
+    return 1.36;
+  }
+  else{
+    return null;
+    console.log("You have no engine.");
+  }
 }
+
+module.expots = Vehicle;
